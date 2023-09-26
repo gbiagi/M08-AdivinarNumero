388657fc031598305f8538b23224fcbdc08fbf0b
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2023-09-26 17:09:58 +0200

    Update

--- app/src/main/java/com/example/adivinarnumero/MainActivity.java
+++ app/src/main/java/com/example/adivinarnumero/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.adivinarnumero;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button1 = findViewById(R.id.button);
+        EditText cajaNum = findViewById(R.id.editTextNumber2);
 
+        final Button button1 = findViewById(R.id.button);
+        button1.setText("¡Clickeame!");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this,cajaNum.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
